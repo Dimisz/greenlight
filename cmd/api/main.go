555db15a26ba3f1ad268fc19d3b2543e0c9c6f84
Vladimir Solovyov <leilyngale@icloud.com>
2025-05-16 15:33:00 +0800

    Add '/v1/healthcheck' endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// version specifies the current version of the app.
+// TODO: autogenerate when deploying
+const version = "1.0.0"
+
+// config struct holds all the config settings for the app.
+type config struct {
+	port int
+	env  string
+}
+
+// application struct holds the dependencies for HTTP handlers, helpers and middleware
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	var cfg config
+
+	// read the value of port and env command-line flags
+	// into the config struct
+	// defaults if no flags provided:
+	// port 4000 and env "development"
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	// initialize a new logger that writes messages to Stdout
+	// prefixed with the current date and time
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// declare an instance of application struct
+	// containing the config struct and the logger
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// declare a new servemux and register routes with handler funcs
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// declare a HTTP server with timeout settings
+	// which listens on the port specified in config
+	// and uses our servemux
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	// start the HTTP server
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 }
