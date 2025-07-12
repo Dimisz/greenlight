3172b57343e91a602488f1c72e88d9bc3b1f95dd
Vladimir Solovyov <leilyngale@icloud.com>
2025-07-12 15:32:29 +0800

    scaffold the app, basic config and healthcheck

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,17 +9,13 @@ import (
 	"time"
 )
 
-// version specifies the current version of the app.
-// TODO: autogenerate when deploying
 const version = "1.0.0"
 
-// config struct holds all the config settings for the app.
 type config struct {
 	port int
 	env  string
 }
 
-// application struct holds the dependencies for HTTP handlers, helpers and middleware
 type application struct {
 	config config
 	logger *log.Logger
@@ -28,38 +24,33 @@ type application struct {
 func main() {
 	var cfg config
 
-	// read the value of port and env command-line flags
-	// into the config struct
-	// defaults if no flags provided:
-	// port 4000 and env "development"
+	// go run ./cmd/api -port=3030 -env=production
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
 
-	// initialize a new logger that writes messages to Stdout
-	// prefixed with the current date and time
+	// initialize new logger
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	// declare an instance of application struct
-	// containing the config struct and the logger
 	app := &application{
 		config: cfg,
 		logger: logger,
 	}
 
-	// declare a HTTP server with timeout settings
-	// which listens on the port specified in config
-	// and uses our servemux
+	// define servemux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// declare server with timeout settings
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
+		Handler:      mux,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
-	// start the HTTP server
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	logger.Printf("strating %s server on %s", cfg.env, srv.Addr)
 	err := srv.ListenAndServe()
 	logger.Fatal(err)
 }
