777787f55672b09f8f05b82d846a3d411d1f1924
Vladimir Solovyov <leilyngale@icloud.com>
2025-09-29 19:36:14 +0800

    Created a Runtime type to customize JSON

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -9,8 +9,11 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
-	js, err := json.Marshal(data)
+type envelope map[string]any
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	// no line prefix "", and tab indent "\t"
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
