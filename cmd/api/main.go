c27e9b9f1ba62823a5a24a94e42c9ad930fba5f5
Vladimir Solovyov <leilyngale@icloud.com>
2025-11-21 20:19:05 +0800

    Scaffold the app

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -35,9 +35,12 @@ func main() {
 		logger: logger,
 	}
 
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
+		Handler:      mux,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
